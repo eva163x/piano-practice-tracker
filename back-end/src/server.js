3a6a19674bd4b9b0325fbf7e1ff6692dc04438e0
Eva Aggarwal <ea196@duke.edu>
2025-05-29 16:57:36 -0400

    connected mongodb

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const sessionInfo = [
     {date: '2025-04-20', upvotes: 0, feedback: []},
@@ -9,6 +10,31 @@ const sessionInfo = [
 const app = express();
 
 app.use(express.json());
+
+app.get("/api/history/:date", async (req, res) => {
+    const { date } = req.params;
+
+    //create a new instance of MongoClient so we can connect to it
+    const uri = 'mongodb://127.0.0.1:27017';
+
+    //basic default setup stuff
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true
+        }
+    });
+
+    await client.connect()
+
+    const db = client.db('full-stack-react-db');
+
+    const mysession = await db.collection('sessions').findOne({ date });
+
+    res.json(mysession)
+
+})
 //create an endpoint
 
 //we start all back-end endpoint reqs w api/
